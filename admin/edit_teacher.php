72420926075d5246115e09b780f8be141f7806d0
ronodir84 <priashdas@84gmail.com>
2024-09-08 22:15:54 +0600

    add this teacher edit section

--- admin/edit_teacher.php
+++ admin/edit_teacher.php
@@ -1,14 +1,81 @@
+<?php ob_start() ?>
+
+
+
 <?php require_once("./adminPartials/Admin_header.php") ?>
 
-<?php 
+<?php
+
+$id = $_GET['id'];
+if (empty($id)) {
+    header("Location:teacher.php");
+}
 if (isset($_POST['update_teacher'])) {
-     
+    $teacher_name = mysqli_real_escape_string($database, $_POST['teacher_name']);
+    $teacher_email = mysqli_real_escape_string($database, $_POST['teacher_email']);
+    $folder = "upload/";
+    $image_file = $_FILES['images']['name'];
+    $file = $_FILES['images']['tmp_name'];
+    $path = $folder . $image_file;
+    $target_file = $folder . basename($image_file);
+    $imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
+    if ($file != '') {
+        //Set image upload size 
+        if ($_FILES["images"]["size"] > 5000000) {
+            $error[] = 'Sorry, your image is too large. Upload less than 500 KB in size.';
+        }
+        //Allow only JPG, JPEG, PNG & GIF 
+        if (
+            $imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+            && $imageFileType != "gif"
+        ) {
+            $error[] = 'Sorry, only JPG, JPEG, PNG & GIF files are allowed';
+        }
+    }
+
+    $subject = mysqli_real_escape_string($database, $_POST['subject']);
+    if (!isset($error)) {
+        if ($file != '') {
+            $res = mysqli_query($database, "SELECT* from teachers WHERE teacher_id = '$id' limit 1");
+            if ($row = mysqli_fetch_array($res)) {
+                $delete_image = $row['teacher_image'];
+            }
+            unlink($folder . $delete_image);
+            move_uploaded_file($file, $target_file);
+            $result = mysqli_query($database, "UPDATE teachers SET  teacher_name='$teacher_name', teacher_email='$teacher_email',   teacher_image ='$image_file',  teacher_subject='$subject'  WHERE teacher_id = '$id'");
+        } else {
+            $result = mysqli_query($database, "UPDATE teachers SET teacher_name ='$teacher_name', teacher_email='$teacher_email',  teacher_subject='$subject'  WHERE teacher_id = '$id'");
+        }
+        if ($result) {
+            header("location:teacher.php?action=saved");
+        } else {
+            echo 'Something went wrong';
+        }
+    }
 }
 
 
+if (isset($error)) {
 
+    foreach ($error as $error) {
+        echo '<div class="message">' . $error . '</div><br>';
+    }
+}
+$res = mysqli_query($database, "SELECT * FROM teachers LEFT JOIN subjects ON teachers.teacher_subject = subjects.subject_id  WHERE teacher_id = '$id' limit 1");
+if ($row = mysqli_fetch_array($res)) {
+    $teacher_image = $row['teacher_image'];
+    $teacher_name = $row['teacher_name'];
+    $teacher_email = $row['teacher_email'];
+    $teacher_category = $row['teacher_category'];
 
+    $teacher_subject = $row['teacher_subject'];
+}
 ?>
+
+<?php if (isset($update_sucess)) {
+    echo 'Image Updated successfully';
+} ?>
+
 
 
 
@@ -36,24 +103,62 @@ if (isset($_POST['update_teacher'])) {
         <form method="post" action="" enctype="multipart/form-data">
             <div class=" pt-3">
 
-                <input name="teacher_name" placeholder="Teacher Name" class=" rounded-md  text-black border-2 border-gray-300 px-4   py-2.5 w-full  focus:ring-1   transition ease-in-out duration-150" type="text">
+                <input name="teacher_name" value="<?php echo $teacher_name ?>" class=" rounded-md  text-black border-2 border-gray-300 px-4   py-2.5 w-full  focus:ring-1   transition ease-in-out duration-150" type="text">
 
             </div>
             <div class=" pt-3">
 
-                <input name="teacher_email" placeholder="Teacher Email" class=" rounded-md  text-black border-2 border-gray-300 px-4   py-2.5 w-full  focus:ring-1   transition ease-in-out duration-150" type="email">
+                <input name="teacher_email" value="<?php echo $teacher_email ?>" class=" rounded-md  text-black border-2 border-gray-300 px-4   py-2.5 w-full  focus:ring-1   transition ease-in-out duration-150" type="email">
 
             </div>
             <div class="flex justify-between pt-3">
                 <input name="images" type="file" class="w-full p-2.5 border-2 border-gray-300 cursor-pointer px-4 rounded-md focus:ring-1   transition ease-in-out duration-150">
-                <i class="fa-solid fa-user text-3xl pt-2  absolute  right-14  cursor-pointer "></i>
+                <img class="w-[52px] h-auto absolute  right-12 p-2" src="upload/<?php echo $teacher_image ?>" alt="">
             </div>
             <div class="pt-3">
+                <?php
+
+                $sql = "SELECT teacher_category FROM  teachers WHERE teacher_id = '$id'";
+                $query = mysqli_query($database, $sql);
+
+                if (isset($_POST['update_teacher'])) {
+                    
+                    $category = mysqli_real_escape_string($database, $_POST['subject_category']);
+                    $sql3 = "UPDATE teachers SET teacher_category = '$category' WHERE teacher_id = '$id'";
+
+                    $query3 = mysqli_query($database, $sql3);
+                }
+
+                
+
+
+                ?>
+
+
+                <select name="subject_category" class=" border-2 border-gray-300 placeholder:text-gray-600  text-gray-600  rounded-md   py-2.5 w-full  px-4 focus:ring-1   transition ease-in-out duration-150" id="">
+                    <?php
+                    while ($category = mysqli_fetch_array($query)) {
 
 
 
-                <select name="category" class=" border-2 border-gray-300 placeholder:text-gray-600  text-gray-600  rounded-md   py-2.5 w-full  px-4 focus:ring-1   transition ease-in-out duration-150" id="">
-                    <option value="">Select Category</option>
+                    ?>
+                        <option value="<?php echo $category['teacher_category'] ?>">
+                            <?php
+                            if ($category['teacher_category'] == 1) {
+                                echo "Principal";
+                            } elseif ($category['teacher_category'] == 2) {
+                                echo "Assistant Professor";
+                            } elseif ($category['teacher_category'] == 3) {
+                                echo "Lecturer";
+                            }
+
+
+                            ?>
+
+
+                        </option>
+
+                    <?php  } ?>
                     <option value="1">Principal</option>
                     <option value="2">Assistant Professor</option>
                     <option value="3">Lecturer</option>
@@ -62,10 +167,38 @@ if (isset($_POST['update_teacher'])) {
             </div>
 
             <div class="pt-3">
-                <select name="subject" class=" border-2 border-gray-300 placeholder:text-gray-600  text-gray-600  rounded-md   py-2.5 w-full  px-4 ">
-                    <option value="">Select Subject</option>
 
-                    <option value=" "></option>
+                <?php
+
+                $sql2 = "SELECT * FROM subjects";
+                $query2 = mysqli_query($database, $sql2);
+
+
+
+                ?>
+                <select name="subject" class=" border-2 border-gray-300 placeholder:text-gray-600  text-gray-600  rounded-md   py-2.5 w-full  px-4 ">
+                    <?php
+                    while ($subject = mysqli_fetch_array($query2)) {
+
+
+
+                    ?>
+
+                        <option value="<?php echo $subject['subject_id'] ?> "
+                            <?php
+                            if ($row['teacher_subject'] == $subject['subject_id']) {
+                                echo "selected";
+                            } else {
+                                echo "";
+                            }
+
+
+                            ?>>
+                            <?php echo $subject['subject_name'] ?>
+
+                        </option>
+
+                    <?php  } ?>
 
 
 
